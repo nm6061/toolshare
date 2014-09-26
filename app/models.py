4ee5d99f325fcc57490516a5d852ae60e134d019
zchi88 <zhihaochi@gmail.com>
2014-09-26 15:10:23 -0400

    Added BlackoutDate and Tool classes in app/models.py.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class BlackoutDate(models.Model):
+    blackoutStart = models.DateField()
+    blackoutEnd = models.DateField()
+
+
+class Tool(models.Model):
+    name = models.CharField(max_length=20)
+    pictureURL = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    # location = Address()
+    status = models.CharField(max_length = 10)
+    blackoutDates = models.ForeignKey(BlackoutDate)
+
+
