ddb6d00ffcf6891a99ea55d2810d3abbcc3b5bca
Nuthan Munaiah <nm6061@g.rit.edu>
2014-09-26 15:22:09 -0400

    Added the User and Address models.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    middle_name = models.CharField(max_length=25)
+    pickup_arrangements = models.CharField(max_length=100)
+    phone_num = models.CharField(max_length=11)
+    reputation = models.PositiveIntegerField(default=0)
+
+    def UpdateUserInfo(self):
+        pass
+
+    def UpdatePreferences(self):
+        pass
+
+        # TODO : DEFINE METHOD AS SOON AS TOOL IS DEFINED
+        # def Register(self, Tool):
+        #    pass
+
+
+class Address(models.Model):
+    apartment_number = models.CharField(max_length=10)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    county = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    country = models.CharField(max_length=50)
+
+    User = models.ForeignKey(User)
