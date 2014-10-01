c818058a46bc1bd5a47aa04b23f4dbce922316d7
Nuthan Munaiah <nm6061@g.rit.edu>
2014-09-30 23:23:24 -0400

    Added code to handle requests to /signup for both GET and POST methods.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response, redirect
 from django.template.context import RequestContext
+from app import forms
 
 
 def home(request):
@@ -9,8 +10,27 @@ def home(request):
 def signin(request):
     if request.method == 'POST':
         return redirect('/dashboard')
-    return render_to_response('signin.html')
+    return render(request, 'signin.html', RequestContext(request))
 
 
 def dashboard(request):
     return render_to_response('dashboard.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        signup_user_form = forms.SignUpUserForm(request.POST)
+        signup_address_form = forms.SignUpAddressForm(request.POST)
+
+        if signup_user_form.is_valid() and signup_address_form.is_valid():
+            # TODO : REVIEW AND REWORK
+            return render(request, 'signin.html', RequestContext(request,{}))
+        else:
+            return render(request, 'signup.html',
+                          RequestContext(request, {'user_form': signup_user_form, 'address_form': signup_address_form}))
+    else:
+        signup_user_form = forms.SignUpUserForm()
+        signup_address_form = forms.SignUpAddressForm()
+        return render(request, 'signup.html',
+                      RequestContext(request, {'user_form': signup_user_form, 'address_form': signup_address_form}))
+
