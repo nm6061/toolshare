d88ac796552df8a8c0608afc95dcc5705a585188
Nuthan Munaiah <nm6061@g.rit.edu>
2014-10-04 20:44:06 -0400

    Moved from using multiple forms - User and Address, to a single form. This is a stop-gap arrangement until I can figure out how to use multiple models in the same form.
     Code to save the model to the database was also added.

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,35 @@
-from app import models
+from django.contrib.admin import widgets
 from app.constants import Constants
-from django.forms import ModelForm, ChoiceField
-from django.contrib.auth.forms import UserCreationForm
+from app import models
+from django import forms
 
 
-class SignUpUserForm(UserCreationForm):
+class SignUpForm(forms.ModelForm):
+    password1 = forms.CharField(widget=forms.PasswordInput, required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput, required=True)
+
+    states = forms.ChoiceField(choices=Constants.US_STATES)
+
     class Meta:
         model = models.User
-        fields = ['first_name', 'last_name', 'email', 'phone_num', 'pickup_arrangements']
+        fields = ['first_name', 'last_name', 'email', 'phone_num', 'pickup_arrangements', 'apt_num', 'street',
+                  'county', 'city', 'zip']
 
+    def clean(self):
+        cleaned_data = super(SignUpForm, self).clean()
 
-class SignUpAddressForm(ModelForm):
-    states = ChoiceField(choices=Constants.US_STATES)
+        if 'password1' in self.cleaned_data and 'password2' in self.cleaned_data:
+            if self.cleaned_data['password1'] != self.cleaned_data['password2']:
+                raise forms.ValidationError("Passwords do not match.")
 
-    class Meta:
-        model = models.Address
+        return self.cleaned_data
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.state = self.cleaned_data['states']
+
+        if commit:
+            user.save()
+
+        return user
