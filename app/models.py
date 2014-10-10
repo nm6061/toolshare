0ab5b88d025bb9b524320f6844fe57c471fc1472
nm6061 <nm6061@g.rit.edu>
2014-10-10 01:37:55 -0400

    Added the choices attribute to the state field to allow picking the value of state from a pre-defined list of states stored in the constants module.

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.core.urlresolvers import reverse
 from django.conf import settings
+import app.constants
 
 
 class System(object):
@@ -32,32 +33,31 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    first_name = models.CharField(max_length=30, blank=False)
+    first_name = models.CharField(max_length=30)
     middle_name = models.CharField(max_length=25, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
 
-    phone_num = models.CharField(max_length=11, default='', blank=True)
+    phone_num = models.CharField(max_length=11, blank=True)
 
-    email = models.EmailField(unique=True, blank=False)
+    email = models.EmailField(unique=True)
 
-    pickup_arrangements = models.TextField(max_length=100, default='', blank=True)
+    pickup_arrangements = models.TextField(max_length=100, blank=True)
 
     reputation = models.PositiveIntegerField(default=0, blank=True)
 
     # TODO : Figure out a way to move address-related fields to a separate model
-    apt_num = models.CharField(max_length=10, default='', blank=True)
-    street = models.CharField(max_length=50, default='', blank=False)
-    city = models.CharField(max_length=50, default='', blank=False)
+    apt_num = models.CharField(max_length=10, blank=True)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
     county = models.CharField(max_length=50, default='', blank=True)
-    state = models.CharField(max_length=2, default='', blank=False)
-    country = models.CharField(max_length=50, default='USA', blank=False)
-    zip = models.CharField(max_length=9, default='', blank=False)
+    state = models.CharField(max_length=2, default='', choices=app.constants.US_STATES)
+    country = models.CharField(max_length=50, default='USA')
+    zip = models.CharField(max_length=9)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name']
     objects = UserManager()
 
-    # Foreign keys
     share_zone = models.ForeignKey(ShareZone)
 
     def Update(self):
