83a8a42050460c2b770ce8279817a97c4012baef
nm6061 <nm6061@g.rit.edu>
2014-10-29 15:34:16 -0400

    A file for custom validators that extend BaseValidator.

---
+++ app/models/validators.py
@@ -0,0 +1,21 @@
+import re
+
+from django.core.exceptions import ValidationError
+from django.core.validators import BaseValidator
+from django.utils.encoding import force_text
+
+
+class AlphabetOnlyValidator(BaseValidator):
+    message = 'can contain only alphabets'
+    code = 'alphabets_only'
+    regex = re.compile('[a-zA-Z]')
+
+    def __init__(self, message=None, code=None):
+        if message:
+            self.message = message
+        if code:
+            self.code = code
+
+    def __call__(self, value):
+        if not self.regex.search(force_text(value)):
+            raise ValidationError(self.message, self.code, params={'value': value})
