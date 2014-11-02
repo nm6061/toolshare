36696086b420ec81384659ae49d5cc3266370afe
amishmehta21 <abm7136@g.rit.edu>
2014-11-02 01:19:14 -0400

    javascript applied for from date to To date  in Borrow.html and works in firefox as well

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -2,7 +2,7 @@ from django import forms
 
 from app import models
 from app.models.reservation import Reservation
-
+import pdb;
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
@@ -39,11 +39,21 @@ class BorrowToolForm(forms.ModelForm):
         self.fields['to_date'].error_messages = {'required': 'Please enter a date for the tool.'}
 
     def clean(self):
+       # pdb.set_trace()
         cleaned_data = super(BorrowToolForm, self).clean()
 
         if 'from_date' in self.cleaned_data and 'to_date' in self.cleaned_data:
-            if self.cleaned_data['from_date'] >= self.cleaned_data['to_date']:
+            if self.cleaned_data['from_date'] >= self.cleaned_data['to_date'] :
                 raise forms.ValidationError("From Date should be before To date")
+
+        if 'from_date' in self.cleaned_data and 'blackoutStart' in self.cleaned_data and 'blackoutEnd'in self.cleaned_data:
+            if self.cleaned_data['from_date'] >= self.clean_Data['blackoutStart'] and self.cleaned_data['from_date'] <= self.cleaned_data['blackoutEnd'] :
+                raise forms.ValidationError("From Date must avoid blackout dates")
+
+        if 'to_date' in self.cleaned_data and 'blackoutStart' in self.cleaned_data and 'blackoutEnd'in self.cleaned_data :
+            if self.cleaned_data['to_date'] >=self.clean_Data['blackoutStart'] and self.cleaned_data['to_date'] <= self.clean_Data['blackoutEnd']:
+               raise forms.ValidationError("To Date must avoid blackout dates")
+
 
         return self.cleaned_data
 
