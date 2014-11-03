46611b1c37c358deb378c13b71d7f687014c7538
Mike Chi <zhihaochi@gmail.com>
2014-11-02 23:28:28 -0500

    Prevented users who are not the owners of the tool from updating the tool

--- app/views/toolManagement.py
+++ app/views/toolManagement.py
@@ -14,6 +14,8 @@ from app import models
 from app.forms.toolRegistration import AddToolForm
 from app.models.reservation import Reservation
 from app.models.tool import Tool
+from django.shortcuts import get_object_or_404
+
 
 @login_required(redirect_field_name='o')
 def registerTool(request):
@@ -30,7 +32,8 @@ def registerTool(request):
                 tool_form.save_m2m()
 
             tool_form = AddToolForm(initial = {'pickupArrangement': currentUser.pickup_arrangements})
-            return render(request, 'registertool.html', RequestContext(request, {'form': tool_form, 'tool_added': True}))
+            messages.success(request, 'yay new tool added!')
+            return render(request, 'registertool.html', RequestContext(request, {'form': tool_form, 'tool_added': True, 'messages':messages}))
         else:
             return render(request, 'registertool.html', RequestContext(request, {'form': tool_form}))
     else:
@@ -41,19 +44,28 @@ def registerTool(request):
 @login_required(redirect_field_name='o')
 def viewTool(request, tool_id):
     currentUser = request.user
-    tooldata = Tool.objects.get(id=tool_id)
+    tooldata = get_object_or_404(Tool, pk=tool_id)
     isToolOwner = tooldata.owner == currentUser
     context = {'tooldata': tooldata, 'isToolOwner':isToolOwner}
     return render(request, 'tool.html', context)
 
+
 @login_required(redirect_field_name='o')
 def updateTool(request, tool_id):
-    tooldata = Tool.objects.get(id=tool_id)
+    tooldata = get_object_or_404(Tool, pk=tool_id)
+    if not tooldata.owner == request.user:
+        return HttpResponse("You don't have permission to edit this tool!")
     if request.method == 'POST':
         tool_form = AddToolForm(request.POST or None, request.FILES or None, instance=tooldata)
         if tool_form.is_valid():
             tool_form.save()
+            name = tooldata.name
             success_url = reverse_lazy("toolManagement:viewTool", kwargs={'tool_id':tool_id})
+
+            #NOTE: the 'safe' extra_tag allows the string to be autoescaped so that links can be processed by the template.
+            #It SHOULD NOT be used unless you need to add a hyperlink to your message!
+            messages.success(request,'You have successfully updated your %s. <br> <br> <a href=".">Click here to go back to your tool.</a>' %name, extra_tags='safe')
+
             return redirect(success_url)
         else:
             return render(request, 'updatetool.html', RequestContext(request, {'form': tool_form}))
