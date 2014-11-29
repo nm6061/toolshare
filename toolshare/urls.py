aaa0248eee3ac94b23d44511eebb79f1f6d7e7ed
nm6061 <nm6061@g.rit.edu>
2014-11-29 15:16:16 -0500

    Moved shed-related URLs into urls/shed.py and included a reference to the module here.

--- toolshare/urls.py
+++ toolshare/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
-from app.views.profile import UserUpdateView
-from app.views.shed import MyShedsView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,6 +9,7 @@ urlpatterns = patterns('',
     #Home Page
     url(r'^$', 'app.views.home', name = 'home'),
     url(r'^account/', include('app.urls.account', namespace='account')),
+    url(r'^shed/', include('app.urls.shed', namespace='shed')),
     #Tools
     url(r'^tool/', include('app.urls.toolManagement', namespace='toolManagement')),
     url(r'^browsetool/$', 'app.views.browsetool', name = 'browsetool'),
@@ -24,7 +23,5 @@ urlpatterns = patterns('',
     url(r'^reservation/(?P<reservation_id>\d+)/reject/$', 'app.views.reject', name = 'reject'),
     url(r'^reservation/(?P<reservation_id>\d+)/cancel/$', 'app.views.cancel', name = 'cancel'),
     url(r'^reservation/(?P<reservation_id>\d+)/cancel/message/$', 'app.views.rejectmessage', name = 'rejectmessage'),
-    #shed
-    url(r'^sheds/$', MyShedsView.as_view(), name='mySheds'),
-    url(r'^sheds/create$', 'app.views.shed.shed_create_view', name='makeShed')
+
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
