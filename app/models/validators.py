209fa4201dd9cd899ee3da03cfca76d0de14dc88
nm6061 <nm6061@g.rit.edu>
2014-11-29 15:45:31 -0500

    Fixed a bug in the AlphabetOnlyValidator that allowed patterns containing at least one alphabet. Added a new validator to validate alphanumeric patterns.

--- app/models/validators.py
+++ app/models/validators.py
@@ -8,7 +8,23 @@ from django.utils.encoding import force_text
 class AlphabetOnlyValidator(BaseValidator):
     message = 'can contain only alphabets'
     code = 'alphabets_only'
-    regex = re.compile('[a-zA-Z]')
+    regex = re.compile('^[a-zA-Z]+$')
+
+    def __init__(self, message=None, code=None):
+        if message:
+            self.message = message
+        if code:
+            self.code = code
+
+    def __call__(self, value):
+        if not self.regex.search(force_text(value)):
+            raise ValidationError(self.message, self.code, params={'value': value})
+
+
+class AlphaNumericOnlyValidator(BaseValidator):
+    message = 'cannot contain special characters'
+    code = 'alphanumeric_only'
+    regex = re.compile('^[a-zA-Z0-9]+$')
 
     def __init__(self, message=None, code=None):
         if message:
