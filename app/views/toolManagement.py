029ad3132863ece03b05fe5d3489a2e302475696
Mike Chi <zhihaochi@gmail.com>
2014-11-30 05:35:32 -0500

    Working on integrating shed with tool

--- app/views/toolManagement.py
+++ app/views/toolManagement.py
@@ -6,27 +6,31 @@ from django.core.urlresolvers import reverse_lazy
 from django.contrib import messages
 from app.forms.toolRegistration import AddToolForm
 from app.models.tool import Tool
+from app.models.shed import Shed
 from app.models import Reservation
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from app import forms
+from django.db.models import Q
 
 
 @login_required()
 def registerTool(request):
     currentUser = request.user
+    sheds = Shed.objects.all()
     if request.method == 'POST':
         tool_form = AddToolForm(request.POST, request.FILES)
+        shedChoice = request.POST.get('shedChoice')
 
         if tool_form.is_valid():
             with transaction.atomic():
                 new_tool = tool_form.save(commit=False)
                 new_tool.owner = currentUser
                 new_tool.status = 'A'
+                if new_tool.location == 'S':
+                    new_tool.shed = Shed.objects.get(pk=shedChoice)
                 new_tool.save()
                 tool_form.save_m2m()
-
-            register_url = reverse_lazy("toolManagement:registerTool")
 
             #NOTE: the 'safe' extra_tag allows the string to be autoescaped so that links can be processed by the template.
             #It SHOULD NOT be used unless you need to add a hyperlink to your message!
@@ -37,10 +41,10 @@ def registerTool(request):
             success_url = reverse_lazy("toolManagement:toolbox")
             return redirect(success_url)
         else:
-            return render(request, 'registertool.html', RequestContext(request, {'form': tool_form}))
+            return render(request, 'registertool.html', RequestContext(request, {'form': tool_form, 'sheds':sheds}))
     else:
         tool_form = AddToolForm(initial = {'pickupArrangement': currentUser.pickup_arrangements})
-        return render(request, 'registertool.html', RequestContext(request, {'form': tool_form}))
+        return render(request, 'registertool.html', RequestContext(request, {'form': tool_form, 'sheds':sheds}))
 
 
 @login_required()
@@ -58,6 +62,8 @@ def updateTool(request, tool_id):
         error_url = reverse_lazy("toolManagement:toolbox")
         messages.error(request,'Error! You do not have permission to edit this tool.<br> <br> <a href=".">Click here to return to your toolbox </a>', extra_tags='safe')
         return redirect(error_url)
+
+    futureRes = tooldata.reservation_set.filter(Q( status='Pending') | Q( status='Approved'))
     if request.method == 'POST':
         if 'updatetool' in request.POST:
             updateform = AddToolForm(request.POST or None, request.FILES or None, instance=tooldata)
@@ -67,27 +73,56 @@ def updateTool(request, tool_id):
 
                 #NOTE: the 'safe' extra_tag allows the string to be autoescaped so that links can be processed by the template.
                 #It SHOULD NOT be used unless you need to add a hyperlink to your message!
-                messages.success(request,'Your tool has been successfully updated! <br> <br> '
+                messages.success(request,'Your tool was successfully updated! <br> <br> '
                                          '<a href=".">Click here to return to the tool details page </a> <br>   OR <br> '
                                          '<a href="/tool/toolbox">Click here to return to your toolbox </a>', extra_tags='safe')
                 return redirect('.')
             else:
-                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata}))
+                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata, 'futureRes':futureRes}))
         elif 'addblackout' in request.POST:
             updateform = AddToolForm(instance=tooldata)
             blackoutform = forms.BlackoutDateForm(tooldata, request.POST)
             if blackoutform.is_valid():
                 blackoutform.save()
-                messages.success(request, 'A request was successfully created. You will receive an email when the owner of '
-                                          'the tool takes an action on your request.')
+                messages.success(request,'Blackout dates have been added to this tool. <br> <br> '
+                                         '<a href=".">Click here to return to the tool details page </a> <br>   OR <br> '
+                                         '<a href="/tool/toolbox">Click here to return to your toolbox </a>', extra_tags='safe')
                 return redirect('.')
             else:
-                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata}))
+                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata, 'futureRes':futureRes}))
+        elif 'deactivate' in request.POST:
+            updateform = AddToolForm(request.POST or None, request.FILES or None, instance=tooldata)
+            blackoutform = forms.BlackoutDateForm(tooldata)
+            if updateform.is_valid():
+                tool = updateform.save(commit=False)
+                tool.status = 'D'
+                tool.save()
+                updateform.save_m2m()
+                messages.success(request,'Your tool has been deactivated. <br> <br> '
+                                         '<a href=".">Click here to return to the tool details page </a> <br>   OR <br> '
+                                         '<a href="/tool/toolbox">Click here to return to your toolbox </a>', extra_tags='safe')
+                return redirect('.')
+            else:
+                updateform = AddToolForm(instance=tooldata)
+                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata, 'futureRes':futureRes}))
+        elif 'activate' in request.POST:
+            updateform = AddToolForm(request.POST or None, request.FILES or None, instance=tooldata)
+            blackoutform = forms.BlackoutDateForm(tooldata)
+            if updateform.is_valid():
+                tool = updateform.save(commit=False)
+                tool.status = 'A'
+                tool.save()
+                updateform.save_m2m()
+                messages.success(request,'Your tool was successfully activated! <br> <br> '
+                                         '<a href=".">Click here to return to the tool details page </a> <br>   OR <br> '
+                                         '<a href="/tool/toolbox">Click here to return to your toolbox </a>', extra_tags='safe')
+                return redirect('.')
+            else:
+                return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata, 'futureRes':futureRes}))
     else:
         updateform = AddToolForm(instance=tooldata)
         blackoutform = forms.BlackoutDateForm(tooldata)
-        return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata}))
-
+        return render(request, 'updatetool.html', RequestContext(request, {'updateform': updateform, 'blackoutform': blackoutform, 'tool':tooldata, 'futureRes':futureRes}))
 
 
 @login_required()
