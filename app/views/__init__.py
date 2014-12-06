3807e771a6279410707d2e69e43fa640c4c056ef
njp6833 <njp6833@g.rit.edu>
2014-12-06 17:36:53 -0500

    change Reservation templates.
    Made changes in all reservation templates.
    reservation_me
    reservation
    Reservationhistory

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -147,7 +147,7 @@ def reservation(request):
     # reservations1 = Reservation.objects.filter(tool__owner=request.user, status='A')
 
 
-    paginator = Paginator(reservations, 5)  # Show 25 reservations per page
+    paginator = Paginator(reservations, 8)  # Show 25 reservations per page
 
     page = request.GET.get('page')
     try:
