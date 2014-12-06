001745029da5ee0afae2df05605fc2f26aa9bcf7
nm6061 <nm6061@g.rit.edu>
2014-12-06 02:25:36 -0500

    Removed comments that were just adding to the length of the file. Added the STATIC_ROOT setting used when collectstatic command is run. Specified localhost in ALLOWED_HOSTS to allow access to this website only from localhost when DEBUG = False.

--- toolshare/settings.py
+++ toolshare/settings.py
@@ -1,16 +1,5 @@
-"""
-Django settings for toolshare project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
-"""
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Application's sign in page
@@ -24,9 +13,6 @@ AUTH_USER_MODEL = 'app.User'
 REQUIRE_ACTIVATION = False
 ACCOUNT_ACTIVATION_DAYS = 0
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'y%d%+9=2@icl39cxv^!c1oosi082)kp-88@@v#h2!0nqzm$5xf'
 
@@ -36,14 +22,10 @@ DEFAULT_FROM_EMAIL = 'administrator@toolshare.com'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost']
 
 # Application definition
-
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -70,13 +52,10 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 ROOT_URLCONF = 'toolshare.urls'
-
 WSGI_APPLICATION = 'toolshare.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -86,25 +65,18 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'US/Eastern'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-
 MEDIA_ROOT = os.path.join(BASE_DIR, "app/media")
 
 from django.contrib.messages import constants as message_constants
